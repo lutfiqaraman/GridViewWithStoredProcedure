46bc7dd47a5844cadadf4e9638492e6efb846b47
lutfiqaraman <lutfiqaraman@gmail.com>
2020-06-08 22:43:57 +0300

    Add project files.

---
+++ ConnectionString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace CRUDWithSPDataGrid
+{
+    public static class ConnectionString
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["defaultConnectionString"].ConnectionString;
+        }
+    }
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CRUDWithSPDataGrid
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        readonly string cs = ConnectionString.GetConnectionString();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string spName = "spCRUDINFO";
+
+            SqlConnection conn = new SqlConnection(cs);
+            SqlCommand cmd = new SqlCommand(spName, conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet dataset = new DataSet();
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Type", "Select");
+            cmd.Parameters.AddWithValue("@Id", DBNull.Value);
+            cmd.Parameters.AddWithValue("@Name", DBNull.Value);
+            cmd.Parameters.AddWithValue("@Age", DBNull.Value);
+            cmd.Parameters.AddWithValue("@City", DBNull.Value);
+            cmd.Parameters.AddWithValue("@Country", DBNull.Value);
+
+            using (conn)
+            {
+                conn.Open();
+
+                adapter.Fill(dataset);
+    
+                gvInfo.DataSource = dataset;
+                gvInfo.DataBind();
+            }
+        }
+    }
+}
